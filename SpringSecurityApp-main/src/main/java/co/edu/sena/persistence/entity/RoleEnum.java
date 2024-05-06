95c4f41a3a3d2255f71685c548d18c7166af3222
Your Name <you@example.com>
2024-05-06 23:13:07 +0200

    Actualizado

---
+++ SpringSecurityApp-main/src/main/java/co/edu/sena/persistence/entity/RoleEnum.java
@@ -0,0 +1,8 @@
+package co.edu.sena.persistence.entity;
+
+public enum RoleEnum {
+    ADMIN,
+    USER,
+    INVITED,
+    DEVELOPER
+}
---
+++ SpringSecurityApp-main/src/main/java/co/edu/sena/persistence/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package co.edu.sena.persistence.repository;
+
+import co.edu.sena.persistence.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserEntityByUsername(String username);
+
+}
